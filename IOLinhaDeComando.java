ea58b03eebcf5069dddea6efd65a796cca43bb21
Arthur Ramos <156632425+arthurdramos@users.noreply.github.com>
2025-08-28 15:53:34 -0300

    learn(ap002-java): encerra apostila

---
+++ IOLinhaDeComando.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class IOLinhaDeComando{
+    public static void main (String... args){
+        //declaraçao de variaveis
+        int a, b;
+        int resultado;
+        Scanner leitor = new Scanner(System.in);
+        //entrada
+        System.out.println("Digite o primeiro valor");
+        a = leitor.nextInt();
+        System.out.println("Digite o segundo valor");
+        b = leitor.nextInt();
+        //processamento
+        resultado = a + b;
+        //saída
+        // 2 + 3 = 5
+        System.out.printf("%d + %d = %d\n", a, b, resultado);
+        }
+
+}
